3da87e952ca05ab1071ddba0ce3c65b77fc39cee
Changbin Park <gh4ck3r@gmail.com>
2024-07-24 22:39:48 +0900

    add Singleton template and its test

---
+++ include/gh4ck3r/singleton.hh
@@ -0,0 +1,61 @@
+#pragma once
+#include <memory>
+#include <mutex>
+#include <sstream>
+#include <stdexcept>
+
+namespace gh4ck3r {
+namespace singleton {
+template <typename T>
+inline T *default_ctor() { return new T{}; }
+
+template <typename T>
+inline void default_dtor(T *p) { delete p; };
+
+template<typename, typename = void>
+constexpr bool is_type_complete_v = false;
+
+template<typename T>
+constexpr bool is_type_complete_v<T, std::void_t<decltype(typeid(T))>> = true;
+
+template <typename T,
+         T *(*create_instance)() = default_ctor<T>,
+         void (*destroy_instance)(T*) = default_dtor<T>>
+class Singleton : public std::shared_ptr<T> {
+  using shared_ptr = std::shared_ptr<T>;
+
+  static inline typename shared_ptr::weak_type weak_instance;
+  static inline auto get_instance() {
+    static std::mutex m;
+    std::lock_guard lk{m};
+    auto sp = weak_instance.lock();
+    if (!sp) {
+      if (T* p = create_instance(); p) {
+        sp.reset(p, destroy_instance);
+        weak_instance = sp;
+      } else {
+        std::ostringstream oss;
+        oss << "failed to create singleton instance";
+        if constexpr (std::is_same_v<void, T>
+            || std::is_scalar_v<T>
+            || is_type_complete_v<T>)
+        {
+          oss << " for " << typeid(T).name();
+        }
+        throw std::runtime_error {oss.str()};
+      }
+    }
+    return sp;
+  }
+
+ public:
+  Singleton() : shared_ptr(get_instance()) {}
+  inline operator auto() const { return shared_ptr::get(); }
+
+  static inline auto use_count() { return weak_instance.use_count(); }
+};
+} // namespace singleton
+
+using singleton::Singleton;
+
+} // namespace gh4ck3r
---
+++ test/singleton.test.cc
@@ -0,0 +1,34 @@
+#include <gh4ck3r/singleton.hh>
+#include <gtest/gtest.h>
+
+#include <dlfcn.h>
+#include <gnu/lib-names.h>
+
+using gh4ck3r::Singleton;
+
+TEST(Singleton, dl)
+{
+  constexpr auto dlopen = [] { return ::dlopen(LIBM_SO, RTLD_LAZY); };
+  constexpr auto dlclose = [] (auto *p) { if (::dlclose(p)) FAIL(); };
+
+  using libm = Singleton<void, dlopen, dlclose>;
+
+  EXPECT_EQ(0, libm::use_count());
+  {
+    libm dlhandle1;
+    EXPECT_EQ(1, libm::use_count());
+    EXPECT_EQ(1, dlhandle1.use_count());
+
+    {
+      libm dlhandle2;
+      EXPECT_EQ(2, libm::use_count());
+      EXPECT_EQ(2, dlhandle2.use_count());
+      EXPECT_EQ(2, dlhandle2.use_count());
+
+      EXPECT_EQ(::dlsym(dlhandle1, "cos"), ::dlsym(dlhandle2, "cos"));
+    }
+    EXPECT_EQ(1, libm::use_count());
+    EXPECT_EQ(1, dlhandle1.use_count());
+  }
+  EXPECT_EQ(0, libm::use_count());
+}

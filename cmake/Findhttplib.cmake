8f7da49aced74771368a3ae37b274c36ed0a1872
Changbin Park <gh4ck3r@gmail.com>
2026-07-04 12:42:45 +0900

    Fallback to the latest httplib if not found on the host

--- cmake/Findhttplib.cmake
+++ cmake/Findhttplib.cmake
@@ -2,7 +2,6 @@ include(FetchContent)
 FetchContent_Declare(
   httplib
   GIT_REPOSITORY https://github.com/yhirose/cpp-httplib.git
-  GIT_TAG v0.14.3
 )
 set(HTTPLIB_INSTALL OFF)
 message(CHECK_START "Downloading httplib")

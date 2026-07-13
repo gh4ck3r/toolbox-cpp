7c45b2a434bdec8d08518b14d0767e82c4d804a9
Changbin Park <gh4ck3r@gmail.com>
2026-07-13 22:59:13 +0900

    add file operations

--- include/gh4ck3r/file.hh
+++ include/gh4ck3r/file.hh
@@ -1,14 +1,40 @@
 #pragma once
-#include <unistd.h>
+#include <filesystem>
+#include <fstream>
+#include <ios>
+#include <iostream>
+#include <iterator>
+#include <memory>
+#include <stdexcept>
 #include <utility>
+#include <vector>
+#include <cstdlib>
+#include <cstring>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
 
 namespace gh4ck3r::file {
 
+using fd_t = int;
+
+namespace fs = std::filesystem;
+
+inline bool is_valid(const fd_t fd) {
+  // fcntl returns -1 on error. If errno is EBADF, the fd is closed/invalid.
+  if (fcntl(fd, F_GETFD) != -1) return true;
+
+  if (errno == EBADF) return false;
+
+  throw std::system_error {errno, std::system_category(),
+    "failed to determine valid file descriptor"};
+}
+
 class unique_fd {
  public:
-  unique_fd(int fd) : fd_(fd) {}
+  unique_fd(fd_t fd) : fd_(fd) {}
   unique_fd(unique_fd&& uf) : fd_(std::exchange(uf.fd_, uninitialied)) {}
-  ~unique_fd() { if (fd_ != uninitialied) { ::close(fd_); } }
+  ~unique_fd() noexcept { if (fd_ != uninitialied) { ::close(fd_); } }
 
   unique_fd() = delete;
   unique_fd(unique_fd&) = delete;
@@ -16,12 +42,251 @@ class unique_fd {
   unique_fd& operator=(const unique_fd&) = delete;
 
   inline operator int() const { return fd_; }
-  inline operator bool() const { return fd_ != uninitialied; }
+  inline operator bool() const { return fd_ != uninitialied && is_valid(fd_); }
 
  private:
-  int fd_;
+  fd_t fd_;
 
-  static inline constexpr int uninitialied = -1;
+  static inline constexpr fd_t uninitialied = -1;
 };
+
+template <typename R = std::vector<uint8_t>>
+R load_file(const fs::path &p) {
+  if (const auto &stat = status(p); is_directory(stat))
+    [[unlikely]] throw std::invalid_argument {"directory can't be loaded: " + p.string()};
+  else if (!fs::is_regular_file(stat))
+    [[unlikely]] throw std::invalid_argument {"file not found: " + p.string()};
+
+  std::ifstream is {p, std::ios::binary};
+  is.unsetf(std::ios::skipws);
+  return {std::istream_iterator<uint8_t>{is}, std::istream_iterator<uint8_t>{}};
+}
+
+template <typename T = fs::path>
+size_t dir_siz(const T dir) {
+  using fs::directory_iterator;
+  return std::distance(directory_iterator{dir}, directory_iterator{});
+}
+
+template <int mode>
+inline bool access(const fs::path &p) {
+  return ::access(p.c_str(), mode) == 0;
+}
+
+inline bool is_readable(const fs::path &path) {
+  return access<F_OK | R_OK>(path);
+};
+inline bool is_writable(const fs::path &path) {
+  return access<F_OK | W_OK>(path);
+}
+inline bool is_executable(const fs::path &path) {
+  return access<F_OK | X_OK>(path);
+}
+
+[[nodiscard]]
+inline fd_t create_anonymous_file() {
+  auto fd = ::open(fs::temp_directory_path().c_str(),
+                 O_TMPFILE | O_RDWR | O_EXCL,
+                 fs::perms::owner_read | fs::perms::owner_write);
+  if (fd == -1) [[unlikely]] throw std::system_error {
+    errno,
+    std::system_category(),
+    "create_anonymous_file failed"};
+
+  return fd;
+}
+
+[[nodiscard]]
+inline std::pair<fd_t, fs::path> create_tempfile(fs::path p = "XXXXXX")
+{
+  if (!p.has_parent_path()) p = fs::temp_directory_path() / p;
+
+  if (constexpr std::string_view suffix {"XXXXXX"};
+      !p.filename().string().ends_with(suffix)) {
+    if (p.has_filename()) p += std::string{"."};
+    p += suffix;
+  }
+
+  auto filepath { p.string() };
+  const auto fd = ::mkostemp(filepath.data(), O_CLOEXEC);
+  if (fd == -1) [[unlikely]] throw std::system_error {
+    errno, std::system_category(), "failed to create tempfile"};
+
+  return {fd, filepath};
+}
+
+class FileTrait
+{
+ public:
+  virtual fs::path path() const = 0;
+  virtual fd_t fd() const = 0;
+  virtual ~FileTrait() noexcept {}
+
+  inline bool is_valid() const {
+    return file::is_valid(fd());
+  }
+};
+
+template <typename T>
+concept FileT = std::is_base_of_v<FileTrait, T>;
+
+template <FileT T>
+class FileWriter
+{
+  std::unique_ptr<FileTrait> file_;
+
+ public:
+  template <typename...ARGS>
+  explicit FileWriter(ARGS&&...args) :
+    file_{std::make_unique<T>(std::forward<ARGS>(args)...)}
+  {}
+  ~FileWriter() noexcept {}
+
+  inline fd_t fd() const { return file_->fd(); }
+  inline fs::path path() const { return file_->path(); }
+  T& file() const { return reinterpret_cast<T&>(*file_); }
+
+  bool write(const uint8_t *p, size_t l) {
+    while (l) {
+      const auto r = ::write(fd(), p, l);
+      if (r < 0) {
+        std::cerr << "failed to write to file: " << std::strerror(errno);
+        return false;
+      }
+      l -= static_cast<size_t>(r);
+    }
+    return true;
+  }
+
+  template <typename C> requires requires (const C &c) {
+    typename C::value_type;
+    requires sizeof(typename C::value_type) == 1;
+
+    c.data();
+    c.size();
+  }
+  inline bool write(const C &v) {
+    return write(reinterpret_cast<const uint8_t*>(v.data()), v.size());
+  }
+
+};
+
+class AnonymousFile : public FileTrait
+{
+  using perms = fs::perms;
+
+ public:
+  AnonymousFile(): fd_ { create_anonymous_file() }
+  {
+    if (fd_) [[likely]] return;
+
+    fs::path p;
+    std::tie(fd_, p) = create_tempfile();
+
+    if (!fs::remove(p)) [[unlikely]]
+      throw std::runtime_error {"failed to unlink AnonymousFile"};
+  }
+
+  ~AnonymousFile() noexcept { ::close(fd_); }
+
+  fs::path path() const final { throw std::logic_error {"This is anonymous file"}; }
+
+ protected:
+  fd_t fd() const final { return fd_; }
+
+ private:
+  fd_t fd_;
+};
+
+class TempFile : public FileTrait
+{
+ public:
+  explicit TempFile(const fs::path k = "XXXXXX") {
+    std::tie(fd_, path_) = create_tempfile(k);
+  }
+  ~TempFile() noexcept try {
+    ::close(fd_);
+    if (exists(path_)) remove(path_);
+  } catch (const std::exception &e) {
+    std::cerr << "exception while destruct TempFile: " << e.what();
+  } catch (...) {
+    std::cerr << "unknown exception while destruct TempFile";
+  }
+
+  fs::path path() const final { return path_; }
+  fd_t fd() const final { return fd_; }
+
+ private:
+  fs::path path_;
+  fd_t fd_ {-1};
+};
+
+class TempDir {
+ public:
+  TempDir() = delete;
+  explicit TempDir(const fs::path &prefix) :
+    path_(build_path(prefix))
+  {}
+
+  ~TempDir() noexcept {
+    remove_all(path_); // TODO: handle exception
+  }
+
+  inline const auto &path() const { return path_; }
+  operator const fs::path& () const { return path(); }
+
+  fs::path operator/(const fs::path &rhs) const {
+    return path() / rhs;
+  }
+
+ private:
+  fs::path build_path(const fs::path &prefix) const {
+    const auto tmpl = (fs::temp_directory_path() / prefix).concat(".XXXXXX").string();
+    if (!::mkdtemp(const_cast<char*>(tmpl.data()))) [[unlikely]]
+      throw std::system_error {errno, std::system_category(),
+        "failed to create temporary directory"};
+    return {tmpl.data()};
+  }
+
+  friend inline std::ostream &operator<<(std::ostream &os, const TempDir &tmpdir) {
+    return os << tmpdir.path();
+  }
+
+ private:
+  const fs::path path_;
+};
+
+template <typename Clock> requires requires {
+  Clock::time_point;
+  Clock::duration;
+  Clock::now();
+}
+typename Clock::time_point cast_to(const fs::file_time_type &filetime) {
+  return std::chrono::time_point_cast<typename Clock::duration>(
+    filetime - fs::file_time_type::clock::now()
+    + Clock::now());
+}
+
+inline void copy_attrs(const fs::path &from, const fs::path &to, const fs::directory_entry &src)
+{
+  if (src.is_directory())
+    for (const auto &e : fs::directory_iterator{src}) copy_attrs(from, to, e);
+
+  const auto &dst = src == from ? to : to / src.path().lexically_relative(from);
+  last_write_time(dst, src.last_write_time());
+}
+
+inline std::error_code copy_all(const fs::path &from, const fs::path &to)
+{
+  std::error_code ec;
+  copy(from, to, fs::copy_options::recursive, ec);
+  if (!ec) copy_attrs(from, to, fs::directory_entry {from});
+
+  if (ec) [[unlikely]]
+    std::clog << "failed to copy " << from << " to " << to
+      << ": " << ec.message();
+
+  return ec;
+}
 
 } // namespace gh4ck3r::file
--- test/file.test.cc
+++ test/file.test.cc
@@ -1,7 +1,30 @@
 #include "gh4ck3r/file.hh"
+#include <filesystem>
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
 
 #include <fcntl.h>
+
+TEST(is_valid, valid)
+{
+  EXPECT_TRUE(gh4ck3r::file::is_valid(STDIN_FILENO));
+  EXPECT_TRUE(gh4ck3r::file::is_valid(STDOUT_FILENO));
+  EXPECT_TRUE(gh4ck3r::file::is_valid(STDERR_FILENO));
+}
+
+TEST(is_valid, invalid)
+{
+  EXPECT_FALSE(gh4ck3r::file::is_valid(-1));
+}
+
+TEST(is_valid, closed)
+{
+  const int fd = ::open(__FILE__, O_RDONLY);
+  ASSERT_NE(-1, fd);
+  EXPECT_TRUE(gh4ck3r::file::is_valid(fd));
+  ::close(fd);
+  EXPECT_FALSE(gh4ck3r::file::is_valid(fd));
+}
 
 class unique_fd_Test : public ::testing::Test {
  protected:
@@ -13,13 +36,197 @@ TEST_F(unique_fd_Test, basic)
   const int fd = ::open(__FILE__, O_RDONLY);
   ASSERT_NE(-1, fd);
 
+  EXPECT_TRUE(gh4ck3r::file::is_valid(fd));
   {
     unique_fd ufd { fd };
     EXPECT_EQ(fd, ufd);
     EXPECT_TRUE(ufd);
   }
 
-  struct stat st;
-  EXPECT_EQ(-1, fstat(fd, &st));
-  EXPECT_EQ(EBADF, errno);
+  EXPECT_FALSE(gh4ck3r::file::is_valid(fd));
+}
+
+TEST(create_tempfile, default)
+{
+  const auto [fd, path] = gh4ck3r::file::create_tempfile();
+  ASSERT_GE(fd, 0);
+  EXPECT_EQ(std::filesystem::file_size(path), 0);
+  EXPECT_TRUE(remove(path));
+
+  EXPECT_EQ(path.parent_path(), std::filesystem::temp_directory_path());
+  EXPECT_TRUE(path.has_filename());
+  EXPECT_FALSE(path.has_extension());
+}
+
+TEST(create_tempfile, named)
+{
+  constexpr std::string_view filename {"hello"};
+  const auto [fd, path] = gh4ck3r::file::create_tempfile(filename);
+  EXPECT_TRUE(remove(path));
+
+  EXPECT_EQ(path.parent_path(), std::filesystem::temp_directory_path());
+  EXPECT_EQ(path.stem(), filename);
+  EXPECT_TRUE(path.has_extension());
+}
+
+TEST(create_tempfile, designated_directory)
+{
+  const auto tempdir = std::filesystem::read_symlink("/proc/self/cwd") / "";
+  ASSERT_FALSE(tempdir.has_filename());
+
+  const auto [fd, path] = gh4ck3r::file::create_tempfile(tempdir);
+  EXPECT_TRUE(remove(path));
+
+  EXPECT_TRUE(equivalent(path.parent_path(), tempdir));
+  EXPECT_TRUE(path.has_filename());
+  EXPECT_FALSE(path.has_extension());
+}
+
+TEST(load_file, default)
+{
+  const auto [fd, path] = gh4ck3r::file::create_tempfile();
+
+  constexpr std::string_view content {"0123456789"};
+  std::ofstream {path} << content;
+
+  EXPECT_EQ(gh4ck3r::file::load_file(path),
+            std::vector<uint8_t>(content.begin(), content.end()));
+
+  EXPECT_TRUE(remove(path));
+}
+
+TEST(load_file, string)
+{
+  const auto [fd, path] = gh4ck3r::file::create_tempfile();
+
+  constexpr std::string_view content {"Hello World!"};
+  std::ofstream {path} << content;
+
+  EXPECT_EQ(gh4ck3r::file::load_file<std::string>(path), content);
+
+  EXPECT_TRUE(remove(path));
+}
+
+TEST(load_file, invalid_argument)
+{
+  EXPECT_THROW(gh4ck3r::file::load_file("/proc"), std::invalid_argument);
+  EXPECT_THROW(gh4ck3r::file::load_file("/dev/stdin"), std::invalid_argument);
+}
+
+TEST(dir_siz, default)
+{
+  const auto tempdir = std::filesystem::read_symlink("/proc/self/cwd") / "";
+  ASSERT_FALSE(tempdir.has_filename());
+
+  const auto before = gh4ck3r::file::dir_siz(tempdir);
+  const auto [fd, path] = gh4ck3r::file::create_tempfile(tempdir);
+  EXPECT_EQ(before + 1, gh4ck3r::file::dir_siz(tempdir));
+  EXPECT_TRUE(remove(path));
+  EXPECT_EQ(before, gh4ck3r::file::dir_siz(tempdir));
+}
+
+TEST(is_readable, default)
+{
+  EXPECT_TRUE(gh4ck3r::file::is_readable(
+    std::filesystem::read_symlink("/proc/self/exe")));
+}
+
+TEST(is_writable, default)
+{
+  EXPECT_TRUE(gh4ck3r::file::is_readable(
+    std::filesystem::read_symlink("/proc/self/exe")));
+}
+
+TEST(is_executable, default)
+{
+  EXPECT_TRUE(gh4ck3r::file::is_executable(
+    std::filesystem::read_symlink("/proc/self/exe")));
+}
+
+TEST(FileWriter, default)
+{
+  struct FileMock : gh4ck3r::file::FileTrait {
+    MOCK_METHOD(std::filesystem::path, path, (), (const override));
+    MOCK_METHOD(gh4ck3r::file::fd_t, fd, (), (const override));
+  };
+  gh4ck3r::file::FileWriter<FileMock> writer {};
+  auto & filemock = writer.file();
+
+  EXPECT_CALL(filemock, fd()).WillOnce(::testing::Return(10));
+  EXPECT_EQ(10, writer.fd());
+
+  EXPECT_CALL(filemock, fd()).WillOnce(::testing::Return(20));
+  EXPECT_EQ(20, writer.fd());
+
+  int pipefd[2];
+  ASSERT_EQ(0, pipe2(pipefd, O_CLOEXEC));
+
+  EXPECT_CALL(filemock, fd()).WillOnce(::testing::Return(pipefd[1]));
+  const std::string s1{"hello"};
+  writer.write(std::string{s1});
+  ASSERT_EQ(0, ::close(pipefd[1]));
+
+  std::string s2(s1.size(), 0x00);
+  ASSERT_EQ(s2.size(), ::read(pipefd[0], s2.data(), s2.size()));
+  ASSERT_EQ(0, ::close(pipefd[0]));
+
+  EXPECT_EQ(s1, s2);
+}
+
+TEST(AnonymousFile, basic)
+{
+  int fd = -1;
+  {
+    gh4ck3r::file::AnonymousFile concrete;
+    gh4ck3r::file::FileTrait &f {concrete};
+
+    EXPECT_THROW(f.path(), std::logic_error);
+    EXPECT_TRUE(f.is_valid());
+
+    fd = f.fd();
+  }
+  EXPECT_FALSE(gh4ck3r::file::is_valid(fd));
+}
+
+TEST(TempFile, basic)
+{
+  int fd = -1;
+  {
+    gh4ck3r::file::TempFile concrete;
+    gh4ck3r::file::FileTrait &f {concrete};
+
+    EXPECT_TRUE(f.is_valid());
+    EXPECT_GE(f.path().filename().string().size(), 6);
+
+    fd = f.fd();
+  }
+  EXPECT_FALSE(gh4ck3r::file::is_valid(fd));
+}
+
+TEST(TempDir, basic)
+{
+  std::filesystem::path tempdir;
+  {
+    const std::filesystem::path name {"temp-prefix"};
+    gh4ck3r::file::TempDir d{ name};
+    EXPECT_TRUE(std::filesystem::is_directory(d));
+
+    EXPECT_EQ(d.path().parent_path(), std::filesystem::temp_directory_path()) << d;
+    EXPECT_EQ(d.path().stem(), name) << d;
+    EXPECT_TRUE(d.path().has_extension()) << d;
+
+    tempdir = d;
+  }
+  EXPECT_FALSE(std::filesystem::exists(tempdir));
+}
+
+TEST(TempDir, extension)
+{
+  const std::filesystem::path name {"temp-prefix.ext"};
+  gh4ck3r::file::TempDir d{ name};
+  EXPECT_TRUE(std::filesystem::is_directory(d));
+
+  EXPECT_EQ(d.path().parent_path(), std::filesystem::temp_directory_path()) << d;
+  EXPECT_EQ(d.path().stem(), name) << d;
+  EXPECT_NE(d.path().extension(), name.extension()) << d;
 }

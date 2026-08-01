2503808794a1484e58d2862d9ad868cfe6916a78
Changbin Park <gh4ck3r@gmail.com>
2026-08-02 00:26:57 +0900

    revise list_head utilities for better readability

--- include/gh4ck3r/list_head.hh
+++ include/gh4ck3r/list_head.hh
@@ -5,18 +5,12 @@
 
 extern "C"
 struct list_head {
-  list_head* next;
-  list_head* prev;
+  list_head *next, *prev;
 };
 
 namespace gh4ck3r::c_compat {
 
 namespace detail {
-
-struct list_head : ::list_head {
-  list_head() { next = prev = this; }
-  list_head(list_head &other) : ::list_head(other) { prev->next = this; }
-};
 
 template <typename T>
 struct container_of;
@@ -31,29 +25,48 @@ struct container_of<MemberType ClassType::*> {
 };
 
 template <auto Member, typename T = detail::container_of_t<decltype(Member)>>
-static constexpr size_t offset() {
+static constexpr size_t offset_of() {
   return reinterpret_cast<size_t>(&(reinterpret_cast<T*>(0)->*Member));
+}
+
+constexpr void list_add_tail (list_head &new_node, list_head &head) {
+  new_node.next = &head;
+  new_node.prev = head.prev;
+  head.prev->next = &new_node;
+  head.prev = &new_node;
 }
 
 } // namespace detail
 
+template <typename C, auto Member>
+struct list_node_binder { C &c_; };
+
+template <typename C, auto Member = &C::list>
+requires (Member == &C::list)
+constexpr auto list_node(C &c) { return list_node_binder<C, Member>{c}; }
+
+template <typename C, auto Member = &C::value_type::list>
+requires (Member == &C::value_type::list)
+constexpr auto list_node(C &c) { return list_node_binder<C, Member>{c}; }
+
 template <auto Member, typename C>
-detail::list_head make_list_head(C &c)
+constexpr auto list_node(C &c) { return list_node_binder<C, Member>{c}; }
+
+template <typename C, auto Member>
+list_head &operator<<(list_head &head, list_node_binder<C, Member> entry)
 {
-  constexpr auto add_tail = [] (list_head &head, list_head &new_node) {
-    list_head* prev = head.prev;
-    new_node.next = &head;
-    new_node.prev = prev;
-    prev->next = &new_node;
-    head.prev = &new_node;
-  };
+  if constexpr (!std::is_same_v<C, detail::container_of_t<decltype(Member)>>) {
+    for (auto &e : entry.c_) {
+      head << list_node<Member, typename C::value_type>(e);
+    }
+  } else {
+    if (!head.prev) [[unlikely]] head.prev = &head;
+    if (!head.next) [[unlikely]] head.next = &head;
 
-  detail::list_head head {};
-  for (auto &e : c) {
-    add_tail(head, *reinterpret_cast<list_head*>(
-      reinterpret_cast<char*>(&e) + detail::offset<Member>()));
+    auto new_node = reinterpret_cast<list_head*>(
+      reinterpret_cast<char*>(&entry.c_) + detail::offset_of<Member>());
+    detail::list_add_tail(*new_node, head);
   }
-
   return head;
 }
 
@@ -76,14 +89,18 @@ class list_head_iterator {
     using pointer           = T*;
     using reference         = T&;
 
+    Iterator() = default;
     explicit Iterator(list_head *p) : p_(p) {}
-    inline reference operator*() { return *operator->(); }
+    inline reference operator*() const { return *operator->(); }
 
-    inline pointer operator->() {
+    inline pointer operator->() const {
       return reinterpret_cast<pointer>(reinterpret_cast<char*>(p_) - OFFSET);
     }
     inline Iterator &operator++() { p_ = p_->next; return *this; }
-    inline bool operator!=(const Iterator &other) { return p_ != other.p_; }
+    inline Iterator operator++(int) { auto tmp = *this; ++(*this); return tmp; }
+    inline bool operator==(const Iterator &other) const { return p_ == other.p_; }
+    // C++20 doesn't need following overload; compiler deduce it.
+    inline bool operator!=(const Iterator &other) const { return p_ != other.p_; }
   };
 
   inline Iterator begin() { return Iterator {head_.next}; }
@@ -92,9 +109,9 @@ class list_head_iterator {
 
 } // namespace v1
 
-inline namespace v2 {
+namespace v2 {
 template <auto Member, typename T = detail::container_of_t<decltype(Member)>>
-struct list_head_iterator {
+class list_head_iterator {
   list_head &head_;
 
  public:
@@ -110,20 +127,39 @@ struct list_head_iterator {
     using pointer           = T*;
     using reference         = T&;
 
+    Iterator() = default;
     explicit Iterator(list_head *p) : p_(p) {}
-    inline reference operator*() { return *operator->(); }
+    inline reference operator*() const { return *operator->(); }
 
-    inline pointer operator->() {
-      return reinterpret_cast<pointer>(
-        reinterpret_cast<char*>(p_) - detail::offset<Member, T>());
+    inline pointer operator->() const {
+      static const auto offset = detail::offset_of<Member, T>();
+      return reinterpret_cast<pointer>(reinterpret_cast<char*>(p_) - offset);
     }
     inline Iterator &operator++() { p_ = p_->next; return *this; }
-    inline bool operator!=(const Iterator &other) { return p_ != other.p_; }
+    inline Iterator operator++(int) { auto tmp = *this; ++(*this); return tmp; }
+    inline bool operator==(const Iterator &other) const { return p_ == other.p_; }
+    // C++20 doesn't need following overload; compiler deduce it.
+    inline bool operator!=(const Iterator &other) const { return p_ != other.p_; }
   };
 
   inline Iterator begin() { return Iterator {head_.next}; }
   inline Iterator end()   { return Iterator {&head_}; }
 };
+
 } // namespace v2
 
-} // namespace gh4ck3r::list_head
+inline namespace v3 {
+
+template <typename T, auto Member = &T::list>
+auto list_head_iterator(list_head &head) {
+  return v2::list_head_iterator<Member, T>(head);
+}
+
+template <auto Member, typename T = detail::container_of_t<decltype(Member)>>
+auto list_head_iterator(list_head &head) {
+  return v2::list_head_iterator<Member, T>(head);
+}
+
+} // namespace v3
+
+} // namespace gh4ck3r::c_compat
--- test/list_head.test.cc
+++ test/list_head.test.cc
@@ -9,68 +9,205 @@ struct list_head_test : ::testing::Test {
       list_head list;
   };
 
-  void add_tail(list_head* head, list_head* new_node)
-  {
-    list_head* prev = head->prev;
-    new_node->next = head;
-    new_node->prev = prev;
-    prev->next = new_node;
-    head->prev = new_node;
+  constexpr auto make_nodes() {
+    std::array nodes {
+      Node {1, "First node", {}},
+      Node {2, "Second node", {}},
+      Node {3, "Third node", {}},
+    };
+    return nodes;
+  }
+
+  struct CustomNode {
+      int id;
+      std::string name;
+      // name of a list_head variable is differ from "list"
+      list_head link;
   };
+
+  constexpr auto make_custom_nodes() {
+    std::array nodes {
+      CustomNode {1, "First node", {}},
+      CustomNode {2, "Second node", {}},
+      CustomNode {3, "Third node", {}},
+    };
+    return nodes;
+  }
 };
+
+TEST_F(list_head_test, list_node)
+{
+  auto nodes = make_nodes();
+
+  list_head head {};
+  ASSERT_FALSE(head.prev);
+  ASSERT_FALSE(head.next);
+
+  using gh4ck3r::c_compat::list_node;
+  for (auto &n : nodes) head << list_node(n);
+
+  ASSERT_EQ(head.prev->next, &head);
+  ASSERT_EQ(head.next->prev, &head);
+
+  auto node = head.next;
+  for (auto i = 0u; i < nodes.size(); ++i, node = node->next) {
+    EXPECT_EQ(node, &nodes[i].list);
+  }
+  EXPECT_EQ(node, &head);
+}
+
+TEST_F(list_head_test, list_custom_node)
+{
+  auto nodes = make_custom_nodes();
+
+  list_head head {};
+  ASSERT_FALSE(head.prev);
+  ASSERT_FALSE(head.next);
+
+  using gh4ck3r::c_compat::list_node;
+  for (auto &n : nodes) head << list_node<&CustomNode::link>(n);
+
+  ASSERT_EQ(head.prev->next, &head);
+  ASSERT_EQ(head.next->prev, &head);
+
+  auto node = head.next;
+  for (auto i = 0u; i < nodes.size(); ++i, node = node->next) {
+    EXPECT_EQ(node, &nodes[i].link);
+  }
+  EXPECT_EQ(node, &head);
+}
+
+TEST_F(list_head_test, list_container)
+{
+  auto nodes1 = make_nodes();
+  std::array nodes2 {
+    Node {4, "Forth node", {}},
+    Node {5, "Fifth node", {}},
+    Node {6, "Sixth node", {}},
+  };
+
+  list_head head {};
+  ASSERT_FALSE(head.prev);
+  ASSERT_FALSE(head.next);
+
+  using gh4ck3r::c_compat::list_node;
+  head  << list_node(nodes1)
+        << list_node(nodes2);
+
+  ASSERT_EQ(head.prev->next, &head);
+  ASSERT_EQ(head.next->prev, &head);
+
+  auto node = head.next;
+  for (auto i = 0u; i < nodes1.size(); ++i, node = node->next) {
+    EXPECT_EQ(node, &nodes1[i].list);
+  }
+  for (auto i = 0u; i < nodes2.size(); ++i, node = node->next) {
+    EXPECT_EQ(node, &nodes2[i].list);
+  }
+  EXPECT_EQ(node, &head);
+}
 
 TEST_F(list_head_test, iterator_v1)
 {
-  std::array nodes {
-    Node {1, "First node", {}},
-    Node {2, "Second node", {}},
-    Node {3, "Third node", {}},
-  };
+  auto nodes = make_nodes();
 
-  list_head my_list { &my_list, &my_list };
-  for (auto &n : nodes) add_tail(&my_list, &n.list);
+  list_head head {};
+  using gh4ck3r::c_compat::list_node;
+  head << list_node(nodes);
 
   auto arr_iter = nodes.begin();
 
   using gh4ck3r::c_compat::v1::list_head_iterator;
-  for (auto& node : list_head_iterator<Node>(my_list)) {
+  for (auto& node : list_head_iterator<Node>(head)) {
+    EXPECT_EQ(&(*arr_iter++), &node);
+  }
+}
+
+TEST_F(list_head_test, iterator_v1_custom)
+{
+  auto nodes = make_custom_nodes();
+
+  list_head head {};
+  using gh4ck3r::c_compat::list_node;
+  head << list_node<&CustomNode::link>(nodes);
+
+  auto arr_iter = nodes.begin();
+
+  using gh4ck3r::c_compat::v1::list_head_iterator;
+  for (auto& node : list_head_iterator<CustomNode, offsetof(CustomNode, link)>(head)) {
     EXPECT_EQ(&(*arr_iter++), &node);
   }
 }
 
 TEST_F(list_head_test, iterator_v2)
 {
-  std::array nodes {
-    Node {1, "First node", {}},
-    Node {2, "Second node", {}},
-    Node {3, "Third node", {}},
-  };
+  auto nodes = make_nodes();
 
-  list_head my_list { &my_list, &my_list };
-  for (auto &n : nodes) add_tail(&my_list, &n.list);
-
-  auto arr_iter = nodes.begin();
-
-  using gh4ck3r::c_compat::v2::list_head_iterator;
-  for (auto& node : list_head_iterator<&Node::list>(my_list)) {
-    EXPECT_EQ(&(*arr_iter++), &node);
-  }
-}
-
-TEST_F(list_head_test, make_list_head)
-{
-  std::array nodes {
-    Node {1, "First node", {}},
-    Node {2, "Second node", {}},
-    Node {3, "Third node", {}},
-  };
-
-  auto head = gh4ck3r::c_compat::make_list_head<&Node::list>(nodes);
+  list_head head {};
+  using gh4ck3r::c_compat::list_node;
+  head << list_node(nodes);
 
   auto arr_iter = nodes.begin();
 
   using gh4ck3r::c_compat::v2::list_head_iterator;
   for (auto& node : list_head_iterator<&Node::list>(head)) {
+    EXPECT_EQ(&(*arr_iter++), &node);
+  }
+}
+
+TEST_F(list_head_test, iterator_v2_custom)
+{
+  auto nodes = make_custom_nodes();
+
+  list_head head {};
+  using gh4ck3r::c_compat::list_node;
+  head << list_node<&CustomNode::link>(nodes);
+
+  auto arr_iter = nodes.begin();
+
+  using gh4ck3r::c_compat::v2::list_head_iterator;
+  for (auto& node : list_head_iterator<&CustomNode::link>(head)) {
+    EXPECT_EQ(&(*arr_iter++), &node);
+  }
+}
+
+TEST_F(list_head_test, iterator_v3)
+{
+  auto nodes = make_nodes();
+
+  list_head head {};
+  using gh4ck3r::c_compat::list_node;
+  head << list_node(nodes);
+
+  using gh4ck3r::c_compat::v3::list_head_iterator;
+  auto iter1 = list_head_iterator<Node>(head);
+  auto iter2 = list_head_iterator<&Node::list>(head);
+  EXPECT_EQ(iter1.begin(), iter2.begin());
+  EXPECT_EQ(iter1.end(), iter2.end());
+
+  auto arr_iter = nodes.begin();
+  for (auto& node : iter1) {
+    EXPECT_EQ(&(*arr_iter++), &node);
+  }
+
+  arr_iter = nodes.begin();
+  for (auto& node : iter2) {
+    EXPECT_EQ(&(*arr_iter++), &node);
+  }
+
+}
+TEST_F(list_head_test, iterator_v3_custom)
+{
+  auto nodes = make_custom_nodes();
+
+  list_head head {};
+  using gh4ck3r::c_compat::list_node;
+  head << list_node<&CustomNode::link>(nodes);
+
+  using gh4ck3r::c_compat::v3::list_head_iterator;
+
+  auto arr_iter = nodes.begin();
+  for (auto& node : list_head_iterator<&CustomNode::link>(head)) {
     EXPECT_EQ(&(*arr_iter++), &node);
   }
 }

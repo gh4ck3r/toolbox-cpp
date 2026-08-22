7766d889b5748cb41354ee5e12b399855992c411
Changbin Park <gh4ck3r@gmail.com>
2026-08-22 12:50:22 +0900

    add asio timer basic example

--- test/asio.test.cc
+++ test/asio.test.cc
@@ -1,3 +1,17 @@
 #include <asio.hpp>
 #include <gtest/gtest.h>
 
+TEST(asio, simple_timer)
+{
+  asio::io_context io;
+
+  bool expired = false;
+  asio::steady_timer timer{io, 1};
+  timer.async_wait([&expired](const auto &ec) {
+    expired = true;
+    EXPECT_FALSE(ec);
+  });
+
+  io.run();
+  EXPECT_TRUE(expired);
+}

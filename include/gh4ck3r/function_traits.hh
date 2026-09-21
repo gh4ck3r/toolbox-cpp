4a149c41a7be7032fb8eaebe57abee301c6e6bd6
Changbin Park <gh4ck3r@gmail.com>
2026-09-21 20:31:49 +0900

    refactor(function_traits): add support for member function pointers, lambdas, and type-based traits
    - Fix concept constraint requires (N < arity) for arg_t index bounds
    - Add specializations for member function pointers (const, noexcept, ref-qualifiers)
    - Add functor / lambda support via operator() extraction
    - Introduce function_trait_t<T> for type-based metaprogramming alongside function_trait<Fn>
    - Add unit tests for member functions, lambdas, and type-based function traits

--- include/gh4ck3r/function_traits.hh
+++ include/gh4ck3r/function_traits.hh
@@ -1,60 +1,96 @@
 #pragma once
 #include <tuple>
 #include <cstddef>
+#include <type_traits>
 
 namespace gh4ck3r::metatype {
 
 namespace detail {
 
-template <typename T>
-struct remove_noexcept { using type = T; };
-
-template <typename R, typename...ARGS>
-struct remove_noexcept<R(ARGS...) noexcept> { using type = R(ARGS...); };
-
-template <typename R, typename...ARGS>
-struct remove_noexcept<R(*)(ARGS...) noexcept> : remove_noexcept<R(ARGS...)> {};
-
-template <typename T>
-using remove_noexcept_t = typename remove_noexcept<T>::type;
-
-template <typename Fn>
-struct function_trait;
-
-template <typename R>
-struct function_trait<R()> {
+template <typename R, typename... ARGS>
+struct function_traits_base {
   using return_type = R;
-
-  static inline constexpr size_t arity = 0;
-
-  using first_argument_type = void;
-  using last_argument_type = void;
-};
-
-template <typename R, typename...ARGS>
-requires requires {sizeof...(ARGS);}
-struct function_trait<R(ARGS...)> {
-  using return_type = R;
-
   static inline constexpr size_t arity = sizeof...(ARGS);
-
   using args_tuple = std::tuple<ARGS...>;
 
-  template <size_t N> requires requires {N < arity;}
+  template <size_t N> requires (N < arity)
   using arg_t = std::tuple_element_t<N, args_tuple>;
 
-  using first_argument_type = arg_t<0>;
-  using last_argument_type = arg_t<sizeof...(ARGS) - 1>;
+  using first_argument_type = std::conditional_t<(arity > 0), std::tuple_element_t<0, std::tuple<ARGS..., void>>, void>;
+  using last_argument_type  = std::conditional_t<(arity > 0), std::tuple_element_t<(arity > 0 ? arity - 1 : 0), std::tuple<ARGS..., void>>, void>;
 };
 
-template <typename R, typename...ARGS>
-struct function_trait<R(*)(ARGS...)> : function_trait<R(ARGS...)> {};
+template <typename T>
+struct function_traits_impl;
+
+// 1. Plain functions
+template <typename R, typename... ARGS>
+struct function_traits_impl<R(ARGS...)> : function_traits_base<R, ARGS...> {};
+
+template <typename R, typename... ARGS>
+struct function_traits_impl<R(ARGS...) noexcept> : function_traits_base<R, ARGS...> {};
+
+// 2. Function pointers
+template <typename R, typename... ARGS>
+struct function_traits_impl<R(*)(ARGS...)> : function_traits_base<R, ARGS...> {};
+
+template <typename R, typename... ARGS>
+struct function_traits_impl<R(*)(ARGS...) noexcept> : function_traits_base<R, ARGS...> {};
+
+// 3. Member function pointers
+template <typename C, typename R, typename... ARGS>
+struct function_traits_impl<R(C::*)(ARGS...)> : function_traits_base<R, ARGS...> {
+  using class_type = C;
+};
+
+template <typename C, typename R, typename... ARGS>
+struct function_traits_impl<R(C::*)(ARGS...) noexcept> : function_traits_base<R, ARGS...> {
+  using class_type = C;
+};
+
+template <typename C, typename R, typename... ARGS>
+struct function_traits_impl<R(C::*)(ARGS...) const> : function_traits_base<R, ARGS...> {
+  using class_type = const C;
+};
+
+template <typename C, typename R, typename... ARGS>
+struct function_traits_impl<R(C::*)(ARGS...) const noexcept> : function_traits_base<R, ARGS...> {
+  using class_type = const C;
+};
+
+template <typename C, typename R, typename... ARGS>
+struct function_traits_impl<R(C::*)(ARGS...) &> : function_traits_base<R, ARGS...> {
+  using class_type = C;
+};
+
+template <typename C, typename R, typename... ARGS>
+struct function_traits_impl<R(C::*)(ARGS...) & noexcept> : function_traits_base<R, ARGS...> {
+  using class_type = C;
+};
+
+template <typename C, typename R, typename... ARGS>
+struct function_traits_impl<R(C::*)(ARGS...) const &> : function_traits_base<R, ARGS...> {
+  using class_type = const C;
+};
+
+template <typename C, typename R, typename... ARGS>
+struct function_traits_impl<R(C::*)(ARGS...) const & noexcept> : function_traits_base<R, ARGS...> {
+  using class_type = const C;
+};
+
+// 4. Functors / Lambdas
+template <typename T>
+requires requires { &T::operator(); }
+struct function_traits_impl<T> : function_traits_impl<decltype(&T::operator())> {};
 
 } // namespace detail
 
+// Type-based function_traits
+template <typename T>
+struct function_trait_t : detail::function_traits_impl<std::remove_cvref_t<T>> {};
+
+// Value/NTTP-based function_traits
 template <auto Fn>
-struct function_trait : detail::function_trait<
-    detail::remove_noexcept_t<decltype(Fn)>>
-{};
+struct function_trait : detail::function_traits_impl<std::remove_cvref_t<decltype(Fn)>> {};
 
 } // namespace gh4ck3r::metatype
--- test/function_traits.test.cc
+++ test/function_traits.test.cc
@@ -7,14 +7,11 @@ namespace gh4ck3r::metatype {
 TEST(function_traitsTest, return_type)
 {
   static_assert(std::is_same_v<function_trait<std::fopen>::return_type, FILE*>);
-
   static_assert(std::is_same_v<function_trait<std::fclose>::return_type, int>);
 
   void foo();
   static_assert(std::is_same_v<function_trait<foo>::return_type, void>);
-
   static_assert(std::is_same_v<function_trait<std::free>::return_type, void>);
-
 }
 
 TEST(function_traitsTest, arguments)
@@ -39,6 +36,50 @@ TEST(function_traitsTest, arguments)
   using free_trait = function_trait<std::free>;
   static_assert(free_trait::arity == 1);
   static_assert(std::is_same_v<free_trait::first_argument_type, void*>);
+}
+
+static void noexcept_func(int, double) noexcept {}
+
+TEST(function_traitsTest, noexcept_function)
+{
+  using trait = function_trait<noexcept_func>;
+  static_assert(trait::arity == 2);
+  static_assert(std::is_same_v<trait::return_type, void>);
+  static_assert(std::is_same_v<trait::first_argument_type, int>);
+  static_assert(std::is_same_v<trait::last_argument_type, double>);
+}
+
+struct Dummy {
+  int method(double, char) const noexcept { return 0; }
+};
+
+TEST(function_traitsTest, member_function_pointer)
+{
+  using trait = function_trait<&Dummy::method>;
+  static_assert(trait::arity == 2);
+  static_assert(std::is_same_v<trait::return_type, int>);
+  static_assert(std::is_same_v<trait::first_argument_type, double>);
+  static_assert(std::is_same_v<trait::last_argument_type, char>);
+  static_assert(std::is_same_v<trait::class_type, const Dummy>);
+}
+
+TEST(function_traitsTest, type_based_trait)
+{
+  using trait = function_trait_t<int(double, char)>;
+  static_assert(trait::arity == 2);
+  static_assert(std::is_same_v<trait::return_type, int>);
+  static_assert(std::is_same_v<trait::first_argument_type, double>);
+  static_assert(std::is_same_v<trait::last_argument_type, char>);
+}
+
+TEST(function_traitsTest, lambda_trait)
+{
+  auto lambda = [](int x, double y) -> bool { return x > y; };
+  using trait = function_trait_t<decltype(lambda)>;
+  static_assert(trait::arity == 2);
+  static_assert(std::is_same_v<trait::return_type, bool>);
+  static_assert(std::is_same_v<trait::first_argument_type, int>);
+  static_assert(std::is_same_v<trait::last_argument_type, double>);
 }
 
 } // namespace gh4ck3r::metatype

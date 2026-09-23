99876873ded597784674b68614f25f8358dc6ae3
Changbin Park <gh4ck3r@gmail.com>
2026-09-23 22:58:00 +0900

    fix(recipe): support move-only callables and perfect forwarding in C++17

--- include/gh4ck3r/recipe.hh
+++ include/gh4ck3r/recipe.hh
@@ -1,15 +1,21 @@
 #pragma once
+#include <functional>
+#include <tuple>
 #include <utility>
 
 namespace gh4ck3r {
 
-template <class F, class...ETC>
-constexpr auto recipe(F f, ETC&&...etc)
-{
-  if constexpr (sizeof...(ETC) == 0) return f;
-  else return [=] (auto...args) {
-    return recipe(etc...)(f(args...));
-  };
+template <typename F, typename... ETC>
+constexpr auto recipe(F&& f, ETC&&... etc) {
+  if constexpr (sizeof...(ETC) == 0) {
+    return std::forward<F>(f);
+  } else {
+    return [f = std::forward<F>(f), etc = std::make_tuple(std::forward<ETC>(etc)...)] (auto&&... args) {
+      return std::apply([&](const auto&... rest_fns) {
+        return recipe(rest_fns...)(std::invoke(f, std::forward<decltype(args)>(args)...));
+      }, etc);
+    };
+  }
 }
 
 } // namespace gh4ck3r
--- test/recipe.test.cc
+++ test/recipe.test.cc
@@ -1,3 +1,4 @@
+#include <string>
 #include "gh4ck3r/recipe.hh"
 #include <gtest/gtest.h>
 
@@ -16,4 +17,33 @@ TEST(recipe, basic)
   // (1 +  ... + 10) * 3 / 5
   EXPECT_EQ(33, r(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
   EXPECT_EQ(fp, "0123");
+}
+
+TEST(recipe, move_only_callable)
+{
+  auto ptr = std::make_unique<int>(10);
+  auto r = recipe(
+    [p = std::move(ptr)](int x) { return *p + x; },
+    [](int x) { return x * 2; }
+  );
+  EXPECT_EQ(40, r(10));
+}
+
+TEST(recipe, perfect_forwarding)
+{
+  struct NonCopyable {
+    int val;
+    NonCopyable(int v) : val(v) {}
+    NonCopyable(const NonCopyable&) = delete;
+    NonCopyable& operator=(const NonCopyable&) = delete;
+    NonCopyable(NonCopyable&&) = default;
+    NonCopyable& operator=(NonCopyable&&) = default;
+  };
+
+  auto r = recipe(
+    [](NonCopyable nc) { return nc.val + 5; },
+    [](int x) { return std::to_string(x); }
+  );
+
+  EXPECT_EQ("15", r(NonCopyable{10}));
 }
